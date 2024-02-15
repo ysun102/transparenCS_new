86f43ebbf008796d9a4e9b5f979983b9a0e02058
Becky Chen <bc103@wellesley.edu>
2024-02-14 21:29:25 -0500

    all of TCS code up to 2/14 9PM

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,3 @@
-// import "<path>/style.scss"
 
 jQuery(document).ready(function ($) {
 
@@ -68,92 +67,113 @@ jQuery(document).ready(function ($) {
                 loop: false
             }
         }
-    })
-
-
-
-    var contentSection = $('.content-section, .main-banner');
-    var navigation = $('nav');
-
-    //when a nav link is clicked, smooth scroll to the section
-    navigation.on('click', 'a', function (event) {
-        event.preventDefault(); //prevents previous event
-        smoothScroll($(this.hash));
-    });
-
-    //update navigation on scroll...
-    $(window).on('scroll', function () {
-        updateNavigation();
-    })
-    //...and when the page starts
-    updateNavigation();
-
-    /////FUNCTIONS
-    function updateNavigation() {
-        contentSection.each(function () {
-            var sectionName = $(this).attr('id');
-            var navigationMatch = $('nav a[href="#' + sectionName + '"]');
-            var navLi = navigationMatch.closest('li');
-            if (($(this).offset().top - $(window).height() / 2 < $(window).scrollTop()) &&
-                ($(this).offset().top + $(this).height() - $(window).height() / 2 > $(window).scrollTop())) {
-                navigationMatch.addClass('active-section');
-                navLi.css('background-color', 'white');
-                navigationMatch.css('color', 'black');
-            }
-            else {
-                navigationMatch.removeClass('active-section');
-                navLi.css('background-color', 'black');
-                navigationMatch.css('color', 'white');
-            }
-        });
-    }
-    function smoothScroll(target) {
-        $('body,html').animate({
-            scrollTop: target.offset().top
-        }, 800);
-    }
-
-
-    $('.button a[href*=#]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 0 }, 500, 'linear');
     });
 
 
-    $('#startwithdrop').on('click', function (event) {
-        event.preventDefault();
-        $('#startwith').addClass("is-active");
+    $('.tabs ul li').click(function () {
+        // Remove 'is-active' class from all tabs
+        $('.tabs ul li').removeClass('is-active');
 
-        $('#startwith .dropdown-content').on('click', function (event) {
-            event.stopPropagation();
-            $('#startwithdrop').find('span:first-child').text($(event.target).text());
-            $('#startwith').removeClass("is-active");
-        });
+        // Add 'is-active' class to the clicked tab
+        $(this).addClass('is-active');
     });
 
+    // $('.tabs ul li a').click(function (e) {
+    //     e.preventDefault();
+    //     var target = $(this).attr('href');
 
-    $('#digdeeperdrop').on('click', function (event) {
-        event.preventDefault();
-        $('#digdeeper').toggleClass("is-active");
+    //     $('.tab-content').hide();
+    //     $(target).show();
+    // });
 
-        $('#digdeeper .dropdown-content').on('click', function (event) {
-            event.stopPropagation();
-            $('#digdeeperdrop').find('span:first-child').text($(event.target).text());
-            $('#digdeeper').removeClass("is-active");
-        });
-    });
+    
 
-    $(document).on('click', function (event) {
-        // Check if the clicked element is not #startwith or its descendants
-        if (!$(event.target).closest('#startwithdrop').length) {
-            event.preventDefault();
-            $('#startwith').removeClass("is-active");
-        }
-        if (!$(event.target).closest('#digdeeperdrop').length) {
-            event.preventDefault();
-            $('#digdeeper').removeClass("is-active");
-        }
-    });
+
+
+
+
+    // var contentSection = $('.content-section, .main-banner');
+    // var navigation = $('nav');
+
+    // //when a nav link is clicked, smooth scroll to the section
+    // navigation.on('click', 'a', function (event) {
+    //     event.preventDefault(); //prevents previous event
+    //     smoothScroll($(this.hash));
+    // });
+
+    // //update navigation on scroll...
+    // $(window).on('scroll', function () {
+    //     updateNavigation();
+    // })
+    // //...and when the page starts
+    // updateNavigation();
+
+    // /////FUNCTIONS
+    // function updateNavigation() {
+    //     contentSection.each(function () {
+    //         var sectionName = $(this).attr('id');
+    //         var navigationMatch = $('nav a[href="#' + sectionName + '"]');
+    //         var navLi = navigationMatch.closest('li');
+    //         if (($(this).offset().top - $(window).height() / 2 < $(window).scrollTop()) &&
+    //             ($(this).offset().top + $(this).height() - $(window).height() / 2 > $(window).scrollTop())) {
+    //             navigationMatch.addClass('active-section');
+    //             navLi.css('background-color', 'white');
+    //             navigationMatch.css('color', 'black');
+    //         }
+    //         else {
+    //             navigationMatch.removeClass('active-section');
+    //             navLi.css('background-color', 'black');
+    //             navigationMatch.css('color', 'white');
+    //         }
+    //     });
+    // }
+    // function smoothScroll(target) {
+    //     $('body,html').animate({
+    //         scrollTop: target.offset().top
+    //     }, 800);
+    // }
+
+
+    // $('.button a[href*=#]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 0 }, 500, 'linear');
+    // });
+
+
+    // $('#startwithdrop').on('click', function (event) {
+    //     event.preventDefault();
+    //     $('#startwith').addClass("is-active");
+
+    //     $('#startwith .dropdown-content').on('click', function (event) {
+    //         event.stopPropagation();
+    //         $('#startwithdrop').find('span:first-child').text($(event.target).text());
+    //         $('#startwith').removeClass("is-active");
+    //     });
+    // });
+
+
+    // $('#digdeeperdrop').on('click', function (event) {
+    //     event.preventDefault();
+    //     $('#digdeeper').toggleClass("is-active");
+
+    //     $('#digdeeper .dropdown-content').on('click', function (event) {
+    //         event.stopPropagation();
+    //         $('#digdeeperdrop').find('span:first-child').text($(event.target).text());
+    //         $('#digdeeper').removeClass("is-active");
+    //     });
+    // });
+
+    // $(document).on('click', function (event) {
+    //     // Check if the clicked element is not #startwith or its descendants
+    //     if (!$(event.target).closest('#startwithdrop').length) {
+    //         event.preventDefault();
+    //         $('#startwith').removeClass("is-active");
+    //     }
+    //     if (!$(event.target).closest('#digdeeperdrop').length) {
+    //         event.preventDefault();
+    //         $('#digdeeper').removeClass("is-active");
+    //     }
+    // });
 
 
 
